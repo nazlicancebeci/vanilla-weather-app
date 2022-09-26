107f70b3c8668e85bf2855d8808cf4dcb9898e25
nazlicancebeci <nazlicancebeci@gmail.com>
2022-09-26 21:20:41 +0200

    icon

--- src/app.js
+++ src/app.js
@@ -14,7 +14,8 @@ function showTemperature(response) {
 
       let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
-    "src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
