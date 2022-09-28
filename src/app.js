45069d55d06fe966f98391dedd06ead74d963096
nazlicancebeci <nazlicancebeci@gmail.com>
2022-09-28 23:39:03 +0200

    forecast styling

--- src/app.js
+++ src/app.js
@@ -15,22 +15,25 @@ function displayForecast(response) {
     if (index < 6) {
       forecastHTML =
         forecastHTML +
-        `
-  <div class="col-2">
-    <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
-    <img src="img/${
-      forecastDay.weather[0].icon
-    }.svg" alt="" width="72" class="forecast-icon"/>
-    <div class="weather-forecast-temperatures">
-      <span class="weather-forecast-temperature-max">${Math.round(
-        forecastDay.temp.max
-      )}°</span>
-            <span class="weather-forecast-temperature-min">${Math.round(
-              forecastDay.temp.min
-            )}°</span>
-       </div>
-  </div>
-  `;
+        `<div class="col-2">
+              <div class="weather-forecast-date">
+                     ${formatDay(forecastDay.dt)}</div>
+                      <div class="card-body">
+                        <img
+                          src="img/${forecastDay.weather[0].icon}.svg"
+                          alt=""
+                          width=""
+                          class = "forecast-icon"
+                        />
+                        <div class="weather-forecast-temperatures">
+                          <span class="weather-forecast-temperature-max">${Math.round(
+                            forecastDay.temp.max
+                          )}°</span>  <span class="weather-forecast-temperature-min">${Math.round(
+          forecastDay.temp.min
+        )}°</span>
+                    </div>
+                  </div>
+                </div>`;;
     }
   });
   forecastHTML = forecastHTML + `</div>`;
